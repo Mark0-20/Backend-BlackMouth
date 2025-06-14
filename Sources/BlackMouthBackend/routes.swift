ac5b10d015823eee9d418a924a73e604012de61d
DanielRl9 <dsp4346895@outlook.com>
2025-06-13 20:51:02 -0600

    creacion del endpoint get by id

--- Sources/BlackMouthBackend/routes.swift
+++ Sources/BlackMouthBackend/routes.swift
@@ -15,7 +15,12 @@ func routes(_ app: Application) throws {
         return menuItem
     }
 
-    
+    app.get("menu_items", ":id") { req async throws -> MenuItems in
+        guard let menuItem = try await MenuItems.find(req.parameters.get("id"), on: req.db) else {
+            throw Abort(.notFound)
+        }
+        return menuItem
+    }
 
     try app.register(collection: TodoController())
 }
